b3885d2f064c4e9039a34236a9133dbc425bb16b
Your Vidhatha <vtvidhathatallapally@gmail.com>
2024-10-07 17:49:38 +0530

    update homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import '@farcaster/auth-kit/styles.css';
+import { AuthKitProvider } from '@farcaster/auth-kit';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const config = {
+  rpcUrl: 'https://mainnet.optimism.io',
+  domain: 'example.com',
+  siweUri: 'https://example.com/login',
+};
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <AuthKitProvider config={config}>
+      <Component {...pageProps} />
+    </AuthKitProvider>
+  );
 }
+
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,30 @@
 import Head from 'next/head'
+import { SignInButton } from '@farcaster/auth-kit';
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="min-h-screen bg-white flex flex-col">
       <Head>
         <title>SIXPENCE</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>SIXPENCE</h1>
-        <p>Discover what your Farcaster friends are buying & get rewarded for sharing it.</p>
-        <button>Connect Farcaster</button>
+      <main className="container bg-[#181A1D] mx-auto max-w-md w-full flex-grow flex flex-col relative">
+        <div className="w-full">
+          <img src="/login.png" alt="SIXPENCE" className="w-full  mx-auto" />
+        </div>
+        <div className="absolute top-[65%] md:top-[70%] bottom-0 left-0 right-0 bg-[#181A1D] rounded-t-[40px] flex flex-col justify-center items-center p-6">
+          <h1 className="text-3xl font-bold mb-4 text-white">SIXPENCE</h1>
+          <p className="mb-8 text-gray-300">
+            Discover what your Farcaster friends are buying & get rewarded for sharing it.
+          </p>
+            <SignInButton
+                  onSuccess={({ fid, username }) =>
+                    console.log(`Hello, ${username}! Your fid is ${fid}.`)
+                  }
+             />
+        </div>
       </main>
     </div>
   )
